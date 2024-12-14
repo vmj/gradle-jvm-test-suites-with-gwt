d7a2455669a0b2929b2e37514fa6c7c1630801b3
Mikko Värri <ext-mikko.varri@oneclinic.fi>
2024-12-14 17:01:13 +0200

    Use Tomcat to serve the GWT client side

---
+++ src/main/java/org/example/client/MyEntryPoint.java
@@ -0,0 +1,10 @@
+package org.example.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class MyEntryPoint implements EntryPoint {
+	public void onModuleLoad() {
+		Window.alert("Hello");
+	}
+}
